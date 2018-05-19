037cd4ad5fcb6062a15fa9d479c30003e1cb5f7e
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-05-19 20:59:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ bot.on('ready', function() {
 bot.login(process.env.TOKEN);
 
 
-bot.on('message', message => {
-    if(message.content.startsWith("teste²")){
-        message.channel.send(`**${message.author.username}** lance le teste avec style`,{
-    })
+if(message.content === "rp!help"){
+
+    const embed = new Discord.RichEmbed()
+    .setTitle("voilà les commandes")
+    .setColor(0xD4FE00)
+    .setDescription("")
+    .setFooter("Si vous avez un probleme... index.js ne répond pas")
+    .addField("Commandes du bot"
+      "bon... c'est un test ok ?")
+
+    message.channel.send({embed});
 }
 }
 )
