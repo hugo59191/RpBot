c675e73e9261a5a776f71f992d2a0caf9c19ea70
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-05-19 20:28:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-need to work on that
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("RpBot!")
+
+bot.on('ready', function() {
+    bot.user.setUsername("RpBot")
+    bot.user.setPresence({ game: { name: 'Griller des mages con'}, status: 'online'}) //en ligne;
+    console.log("Connected")
+});
+
+bot.login(process.env.TOKEN);
+
+
+bot.on('message', message => {
+    if(message.content.startsWith("teste²")){
+        message.channel.send(`**${message.author.username}** lance le teste avec style`,{
+    })
+}
+}
+)
